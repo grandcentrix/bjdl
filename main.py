22d06fe3387451488212d9f52a5865ed3b5a0fea
René Beckmann <rene.beckmann@grandcentrix.net>
2023-10-19 15:14:54 +0200

    Remove author/(c) tag in favor of docstring

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ from requests.auth import AuthBase
 import getpass
 import json
 
-__author__ = "René Beckmann"
-__copyright__ = "Copyright 2023, grandcentrix GmbH"
+"""
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.
+If not, see <https://www.gnu.org/licenses/>.
+
+@author René Beckmann
+@copyright 2023, grandcentrix GmbH
+"""
 
 BASE_URL = "https://api.bluejeans.com"
 
